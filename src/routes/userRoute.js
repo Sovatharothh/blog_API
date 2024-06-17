2f6ff40d805562c19cab9a30ad2b32a4e10953c2
Sovatharothh <116065704+Sovatharothh@users.noreply.github.com>
2024-06-17 11:23:35 +0700

    add status&message

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,28 +1,42 @@
 const express = require('express');
-const { check, body } = require('express-validator');
+const { check, validationResult } = require('express-validator');
 const { registerUser, loginUser, resetPassword } = require('../controllers/userController');
 
 const router = express.Router();
 
-// register new user
 router.post('/register', [
-    check('firstName', 'FirstName is required').notEmpty(),
-    check('lastName', 'LastName is required').notEmpty(),
-    check('email', 'Please include a valid email').notEmail(),
-    check('password', 'Password must be at least 6 long').isLength({min: 6}),
+    check('firstName', 'First name is required').notEmpty(),
+    check('lastName', 'Last name is required').notEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password must be 6 or more characters').isLength({ min: 6 }),
+], (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ status: 400, message: errors.array() });
+    }
+    next();
+}, registerUser);
 
-], registerUser);
-
-// user login
 router.post('/login', [
     check('email', 'Please include a valid email').isEmail(),
     check('password', 'Password is required').exists(),
-], loginUser);
+], (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ status: 400, message: errors.array() });
+    }
+    next();
+}, loginUser);
 
-// reset password
-router.post('reset', [
-    check('email', 'Please inculde a valid email').isEmail(),
-    check('new password', 'New password must be at least 6 num/character long').isLength({min: 6}),
-], resetPassword);
+router.post('/reset-password', [
+    check('email', 'Please include a valid email').isEmail(),
+    check('newPassword', 'New password must be 6 or more characters').isLength({ min: 6 }),
+], (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ status: 400, message: errors.array() });
+    }
+    next();
+}, resetPassword);
 
 module.exports = router;
