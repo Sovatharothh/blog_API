a84990a83218476ede1db7ae09ff09a17a774e4d
Sovatharothh <116065704+Sovatharothh@users.noreply.github.com>
2024-06-17 10:41:26 +0700

    add userRoute

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,2 +1,28 @@
 const express = require('express');
-const controller = require('../controllers/userCotroller');
+const { check, body } = require('express-validator');
+const { registerUser, loginUser, resetPassword } = require('../controllers/userController');
+
+const router = express.Router();
+
+// register new user
+router.post('/register', [
+    check('firstName', 'FirstName is required').notEmpty(),
+    check('lastName', 'LastName is required').notEmpty(),
+    check('email', 'Please include a valid email').notEmail(),
+    check('password', 'Password must be at least 6 long').isLength({min: 6}),
+
+], registerUser);
+
+// user login
+router.post('/login', [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists(),
+], loginUser);
+
+// reset password
+router.post('reset', [
+    check('email', 'Please inculde a valid email').isEmail(),
+    check('new password', 'New password must be at least 6 num/character long').isLength({min: 6}),
+], resetPassword);
+
+module.exports = router;
