6468bd6418c02046c66f39b630c905c7831e879b
Sovatharothh <116065704+Sovatharothh@users.noreply.github.com>
2024-06-26 11:39:20 +0700

    add blog router

--- src/routes/blogRoute.js
+++ src/routes/blogRoute.js
@@ -1,1 +1,11 @@
 const express = require('express');
+const { createBlog, getAllBlog, getBlogById, updateBlogById, deleteBlogById } = require('../controllers/blogController');
+
+const router = express.Router();
+router.post('/', createBlog);
+router.get('', getAllBlog);
+router.get('/:id', getBlogById);
+router.put('/:id', updateBlogById);
+router.delete('/:id', deleteBlogById);
+
+module.exports = router;
